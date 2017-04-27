1dd30e592e3bfd032e2d39e5b4379fda01687e44
Maxim Glazkov <glazkov.maxim@gmail.com>
2017-04-27 10:54:15 +1000

    BZR-849

--- whisperer-web/src/main/js/configureStore.js
+++ whisperer-web/src/main/js/configureStore.js
@@ -1,17 +1,13 @@
 import { applyMiddleware, createStore } from "redux";
 import thunkMiddleware from "redux-thunk";
-import { createLogger } from "redux-logger";
 import rootReducer from "./reducers";
-
-const loggerMiddleware = createLogger();
 
 export default function configureStore(preLoadedState) {
 	return createStore(
 		rootReducer,
 		preLoadedState,
 		applyMiddleware(
-			thunkMiddleware,
-//			loggerMiddleware
+			thunkMiddleware
 		)
 	);
 }
